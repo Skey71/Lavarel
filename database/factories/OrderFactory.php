2651c5feb5e6b51099f92db79eb4de9fa9ed0abe
ascf <asdac>
2022-12-07 21:42:24 +0300

    ls9

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Order::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => '+1 (683) 739-8782',
+            'description' => $this->faker->text(300),
+        ];
+    }
+}
